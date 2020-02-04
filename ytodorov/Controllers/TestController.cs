6829cbfd3c57c9007418c41bad57f05c204ae008
ytodorov <ytodorov@ytodorov.com>
2020-02-04 15:21:11 +0200

    TestController.cs

---
+++ ytodorov/Controllers/TestController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using Microsoft.AspNetCore.Mvc;
+using ytodorov.Models;
+
+namespace ytodorov.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult GetTime()
+        {
+            Thread.Sleep(3000);
+            return Json(DateTime.Now);
+        }
+    }
+}
