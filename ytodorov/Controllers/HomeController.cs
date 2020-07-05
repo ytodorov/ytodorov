a79f299b47ec163696b860a7bb8f4ebb02cf4922
ytodorov <ytodorov@ytodorov.com>
2020-07-05 20:05:23 +0300

    new empty site - placeholder for different services.

--- ytodorov/Controllers/HomeController.cs
+++ ytodorov/Controllers/HomeController.cs
@@ -1,44 +1,23 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using ytodorov.Models;
+using Microsoft.Extensions.Logging;
 
 namespace ytodorov.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
         public IActionResult Index()
-        {
-            var view = View(); 
-            view.ViewData.Add("test", 1);
-
-            return view;
-        }
-
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
-        public IActionResult Chat()
-        {
-            return View();
-        }
-
-        public IActionResult Map()
-        {
-            return View();
-        }
-
-        public IActionResult AzureCosmosDb()
         {
             return View();
         }
@@ -46,12 +25,6 @@ namespace ytodorov.Controllers
         public IActionResult Privacy()
         {
             return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
--- ytodorov/Startup.cs
+++ ytodorov/Startup.cs
@@ -1,13 +1,15 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Rewrite;
+using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using ytodorov.Models;
+using Microsoft.Extensions.Hosting;
 
-namespace ytodorov
+namespace NewWebAppTest
 {
     public class Startup
     {
@@ -21,35 +23,11 @@ namespace ytodorov
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.Configure<CookiePolicyOptions>(options =>
-            {
-                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-                options.CheckConsentNeeded = context => true;
-                options.MinimumSameSitePolicy = SameSiteMode.None;
-            });
-
-
-            services.AddMvc(
-                x =>
-                {
-                    x.Filters.Add<ErrorHandlingExceptionFilter>();
-                }
-                )
-                .SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
-
-            //services.AddSingleton<IDocumentDBRepository<Item>>(new DocumentDBRepository<Item>());
-
-            //services.AddHttpsRedirection(options =>
-            //{
-            //    options.HttpsPort = 443;
-            //    options.RedirectStatusCode = 302;
-            //});
-            
-            
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
@@ -58,24 +36,21 @@ namespace ytodorov
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                //app.UseHsts();
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
             }
-            //if (!env.IsDevelopment())
-            //{
-            //app.UseHttpsRedirection();
-            //}
-
-            app.UseRewriter(new RewriteOptions().AddRedirectToWww());
-
-
+            app.UseHttpsRedirection();
             app.UseStaticFiles();
-            app.UseCookiePolicy();
 
-            app.UseMvc(routes =>
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
             {
-                routes.MapRoute(
+                endpoints.MapControllerRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
